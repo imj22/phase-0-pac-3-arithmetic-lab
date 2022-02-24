23162d1ae4a31e8750f9fc018b4ed36a7cebd8a9
imj22 <98693440+imj22@users.noreply.github.com>
2022-02-24 21:23:33 +0100

    commit tree e0d51eb1440463a6c5616db2986bebb471a39e81

---
+++ index.js
@@ -0,0 +1,57 @@
+// test 1-4: confirming that add, subtract, multiply, and divide are valid functions
+
+function add() {};
+
+function subtract() {};
+
+function multiply() {};
+
+function divide() {};
+
+// test 5-8: basic ADDition, SUBTRACTion, MULTIPL(Y)ication, and DIVID(E)sion using their respective functions :T
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+// test 9-10: showing basic understanding of increment and decrement via implementation
+
+function increment(a) {
+    return a + 1;
+}
+
+function decrement(a) {
+    return a - 1;
+}
+
+// test 11: parsed integers
+
+
+function makeInt(a) {
+    return parseInt(a);
+}
+
+// test 12, 13:  parsed integers with base of 10, return NaN. technically satisfies 11, 12, 13. 
+
+function makeInt(a) {
+    return parseInt(a, 10);
+}
+
+// test 14-15: parseFloat (decimal stuff)
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}l
+
